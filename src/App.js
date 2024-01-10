29306210142626fbc3f703c3547e560faa5b446e
bryanzhou <bzhou07@students.cs.ubc.ca>
2024-01-09 23:10:45 -0800

    sync up commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Timer from './Time';
+
+function App() {
+  return (
+    <main>
+      <Timer />
+    </main>
+  );
+}
+
+export default App;
---
+++ src/Time.js
@@ -0,0 +1,22 @@
+import { CircularProgressbar, buildStyles} from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+
+const red = '#d92314';
+const green = '#3c913d';
+
+function Timer() {
+    return (
+        <div>
+            <CircularProgressbar value={60} text={`60%`} styles={buildStyles({
+                textColor:'#fff',
+                pathColor: red,
+                tailColor:'#b8bab8',
+            })} />
+            <div>
+                <PlayButton />
+            </div>
+        </div>
+    );
+}
+
+export default Timer;
