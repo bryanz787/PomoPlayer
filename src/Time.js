e8ef16b6d530a0ffad170343c54230040f22c9fd
bryanzhou <bzhou07@students.cs.ubc.ca>
2024-01-11 13:35:15 -0800

    added navigation button skeletons

--- src/Time.js
+++ src/Time.js
@@ -1,6 +1,8 @@
 import { CircularProgressbar, buildStyles} from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
+import PauseButton from './PauseButton';
 import PlayButton from './PlayButton';
+import SettingsButton from './SettingsButton';
 
 const red = '#d92314';
 const green = '#3c913d';
@@ -13,8 +15,12 @@ function Timer() {
                 pathColor: red,
                 tailColor:'#b8bab8',
             })} />
-            <div>
+            <div style={{marginTop:'20 px'}}>
                 <PlayButton />
+                <PauseButton />
+            </div>
+            <div style={{marginTop:'20 px'}}>
+                <SettingsButton />
             </div>
         </div>
     );
