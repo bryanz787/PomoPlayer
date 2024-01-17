0f8e4927564b9586946751413b6c88435003c1ad
bryanzhou <bzhou07@students.cs.ubc.ca>
2024-01-17 01:00:17 -0800

    updated ui, added music playing option

--- src/Time.js
+++ src/Time.js
@@ -5,21 +5,32 @@ import PlayButton from './PlayButton';
 import SettingsButton from './SettingsButton';
 import { useContext, useState, useEffect, useRef } from 'react';
 import SettingsContext from './SettingsContext';
+import PlayMusicButton from './PlayMusicButton';
+import AAIS from './music/Adrift-Among-Infinite-Stars.mp3';
+import LAF from './music/Lost-and-Found.mp3';
+import MR from './music/Morning-Routine.mp3';
+import SW from './music/Silent-Wood.mp3';
+import SC from './music/Storm-Clouds.mp3';
 
 const red = '#d92314';
 const green = '#3c913d';
 
 function Timer() {
+    const audios = [AAIS, LAF, MR, SW, SC];
 
     const settingsInfo = useContext(SettingsContext);
 
     const [isPaused, setIsPaused] = useState(true);
     const [mode, setMode] = useState('work');
     const [secondsLeft, setSecondsLeft] = useState(0);
+    const [play, setPlay] = useState(false);
+    const [songCounter, setSongCounter] = useState(Math.floor(Math.random() * audios.length)); //starts from random track out of assortment
 
     const secondsLeftRef = useRef(secondsLeft);
     const isPausedRef = useRef(isPaused);
     const modeRef = useRef(mode);
+
+    const audioRef = useRef(new Audio(audios[songCounter % audios.length]));
 
     function switchMode() {
         const nextMode = modeRef.current === 'work' ? 'break' : 'work';
@@ -50,7 +61,7 @@ function Timer() {
             if (isPausedRef.current) {
                 return;
             }
-            if (secondsLeftRef.current == 0) {
+            if (secondsLeftRef.current === 0) {
                 return switchMode();
             }
 
@@ -59,6 +70,18 @@ function Timer() {
 
         return () => clearInterval(interval);
     }, [settingsInfo]);
+
+    useEffect(() => {
+        audioRef.current.addEventListener('ended', () => {
+            setSongCounter((prevCounter) => prevCounter + 1); // Switch to the next song when the audio ends
+            audioRef.current.src = audios[songCounter % audios.length]; // Update the source to the next song
+            audioRef.current.play();
+        });
+    
+        return () => {
+            audioRef.current.removeEventListener('ended', () => {});
+        };
+    }, [songCounter, audios]);
 
     const total = mode === 'work' ? settingsInfo.workMinutes * 60 : settingsInfo.breakMinutes * 60;
     const percentText = Math.round(secondsLeft / total * 100);
@@ -84,6 +107,18 @@ function Timer() {
             </div>
             <div style={{marginTop:'20 px'}}>
                 <SettingsButton onClick = {() => settingsInfo.setShowSettings(true)}/>
+            </div>
+            <div style={{marginTop:'20 px', paddingTop: '15px'}}>
+                <PlayMusicButton
+                    onClick = {() => {
+                        if (play) {
+                            audioRef.current.play();
+                        } else {
+                            audioRef.current.pause();
+                        }
+                        setPlay((prev) => !prev);
+                    }}
+                />
             </div>
         </div>
     );
